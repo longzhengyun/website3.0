ecf5512ffdb912539eb571240a2544ee8f7711d6
lijie <jiela@Ctrip.com>
2015-12-23 19:34:56 +0800

    my website

--- ui/js/javascript.js
+++ ui/js/javascript.js
@@ -3,27 +3,25 @@ require.config({
 	baseUrl: 'js/module'
 });
 
-//加载模块-rem 字号比例
-require(['autoDPI'], function(){
-	var docEl = document.documentElement;
-	var clientWidth = docEl.clientWidth;
-	if (!clientWidth) return;
-		docEl.style.fontSize = 100 * (clientWidth / 320) + 'px';
-});
-
 //加载模块-判断运行环境
 require(['operatingSystem', 'eventUtil'], function(operatingSystem, eventUtil){
 	var docBody = document.getElementsByTagName('body')[0];
-	if(operatingSystem.versions.ios == true) {
+	if(operatingSystem.versions.ios == true || operatingSystem.versions.android == true) {
 		//iOS环境
-		eventUtil.addClass(docBody, 'ios');
-	}else if(operatingSystem.versions.android == true) {
-		//Android环境
-		eventUtil.addClass(docBody, 'android');
+		eventUtil.addClass(docBody, 'mobile');
+
+		//加载模块-rem 字号比例
+		require(['autoDPI'], function(){
+			var docEl = document.documentElement;
+			var clientWidth = docEl.clientWidth;
+			if (!clientWidth) return;
+				docEl.style.fontSize = 100 * (clientWidth / 320) + 'px';
+		});
 	}else{
 		//PC环境
 		eventUtil.addClass(docBody, 'pc');
 	};
+
 	if(operatingSystem.versions.weixin == true) {
 		//微信环境
 		eventUtil.addClass(docBody, 'weixin');
