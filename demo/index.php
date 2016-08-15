b0bb32f9e311d4a3eb947e49b4e60fe0be8309c3
jary0419 <jary0419@163.com>
2016-08-15 19:21:40 +0800

    demo modify

--- demo/index.php
+++ demo/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
 <title>demo</title>
 <link rel="stylesheet" href="css/reset.css">
 <link rel="stylesheet" href="css/demo.css">
@@ -9,24 +10,6 @@
 <script src="js/lib/browser.min.js"></script>
 </head>
 <body>
-<?php 
-date_default_timezone_set('UTC');
-
-$dir = '/Users/jary/website/ui/';
-
-if(is_dir($dir)){
-	if($dh = opendir($dir)){
-		while (($file = readdir($dh))!=false) {
-			$filePath = $dir.$file;
-			preg_match("/<head.*>(.*)<\/head>/smUi", $file, $fileTitle); 
-			$fmt = filemtime($filePath);
-			echo "<span style='color:#999'>(".date("Y-m-d H:i:s", $fmt).")</span><span style='color:#f00'>".$fileTitle."</span>".$filePath."<br/>";
-		}
-		closedir($dh);
-	}
-}
-?>
-
 <div class="section-wrap">
 	<!-- <div class="enter"><input type="text" name="" id="catalog"></div> -->
 	<ul class="list">
@@ -34,6 +17,18 @@ if(is_dir($dir)){
 	</ul>
 </div>
 <div id="content"></div>
+<!-- <script type="text/javascript" src="http://apps.bdimg.com/libs/jquery/1.9.0/jquery.js"></script> -->
+<!-- <script type="text/javascript">
+function getAudioFileList(){  
+  $.get("audio.php",function(data){  
+    console.log(data);  
+  },"json");  
+} 
+getAudioFileList();
+</script> -->
+
+<!-- http://reactjs.cn/react/tips/initial-ajax.html react教程 -->
+
 <script type="text/babel" src="js/javascript.js"></script>
 </body>
 </html>
--- demo/js/javascript.js
+++ demo/js/javascript.js
@@ -1,4 +1,32 @@
-ReactDOM.render(
-	<h1>hello, php!</h1>,
-	document.getElementById('content')
+var UserGist = React.createClass({
+	getInitiaState: function(){
+		return {
+			username:'',
+			lastGistUrl:''
+		}
+	},
+	componentDidMount: function(){
+		$.get(this.props.source, function(result){
+			var lastGist = result[0];
+			if(this.isMounted()){
+				this.setState({
+					username: lastGist.owner.login,
+					lastGistUrl: lastGist.html_url
+				});
+			}
+		}.bind(this));
+	},
+	render: function(){
+		return (
+			<div>
+				{this.state.username} is last gist is
+				<a herf={this.state.lastGistUrl}>here</a>
+			</div>
+		)
+	}
+});
+
+React.render(
+	<UserGist source="https://api.github.com/users/octocat/gists" />,
+	mountNode
 );
