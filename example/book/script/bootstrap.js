6a7e2fda06cd0127096111b0e7356e425f8a662d
jary0419 <jary0419@163.com>
2016-10-12 11:29:56 +0800

    structure modify

--- example/book/script/bootstrap.js
+++
@@ -1,5 +0,0 @@
-define(['require', 'angular', 'app', 'routes', 'controllers/asideController', 'controllers/mainController', 'controllers/detailController', 'controllers/addController', 'controllers/loginController'], function (require, angular) {
-    require(['domReady!'], function (document) {
-        angular.bootstrap(document, ['app']);
-    });
-});
